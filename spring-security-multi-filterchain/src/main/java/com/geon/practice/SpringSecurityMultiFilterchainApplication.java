3670ca57ec486a13743670a70144e0be85b691ab
leegc <gclee@da-rum.com>
2024-12-03 21:39:29 +0900

    프로젝트 생성

---
+++ spring-security-multi-filterchain/src/main/java/com/geon/practice/SpringSecurityMultiFilterchainApplication.java
@@ -0,0 +1,13 @@
+package com.geon.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMultiFilterchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMultiFilterchainApplication.class, args);
+	}
+
+}
